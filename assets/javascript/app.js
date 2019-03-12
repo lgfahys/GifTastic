5b9248121f49b6de244fd3220114426ae543daf0
lgfahys <lindsey@codeasite.com>
2019-03-12 16:25:43 -0400

    initial site files and directories

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+// create an array of buttons that hold user input and a select list of meme categories
+
+// on button click, clear gif holder div and append 10 static memes and their rating in the gif holder div
+
+// on gif click make gif animate, on second click make static
+
+// add form for user to add a new meme category
