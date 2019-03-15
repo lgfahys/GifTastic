ebe31b4b6ba26e2f30bbb493a47ccb3525521e1a
lgfahys <lindsey@codeasite.com>
2019-03-15 15:27:19 -0400

    added comments to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,8 @@
-// create an array of buttons that hold user input and a select list of meme categories
-
-// on button click, clear gif holder div and append 10 static memes and their rating in the gif holder div
-
-// on gif click make gif animate, on second click make static
-
-// add form for user to add a new meme category
-
 $(document).ready(function () {
     // creating a memes array to hold all meme categories
     var memes = ["kermit", "the office", "pepe the frog", "mr. krabs", "doge"];
 
-    // creating a function
+    // creating a displayMemes function that uses giphy api to get 10 meme gifs and their rating and appends them to the #memes div
     function displayMemes() {
 
         var meme = $(this).attr("data-name");
@@ -23,10 +15,10 @@ $(document).ready(function () {
         }).then(function (response) {
             // emptying the memes div each time user clicks a meme button
             $("#memes").empty();
-            // looping through 10 gifs and appending them to the memes div
+            // looping through 10 gifs and appending the gif url/rating to the #memes div
             for (var i = 0; i < 10; i++) {
                 var rating = response.data[i].rating;
-                var ratingDisplay = $("<p>").text("Rating: " + rating);
+                var ratingDisplay = $("<p>").html("Rating: " + rating);
                 $("#memes").prepend(ratingDisplay);
                 var gifURL = response.data[i].images.original.url;
                 var gif = $("<img>").attr("src", gifURL);
@@ -35,6 +27,7 @@ $(document).ready(function () {
         });
     }
 
+    // when the add meme button is clicked..
     $("#add-meme").on("click", function (event) {
         event.preventDefault();
         var meme = $("#meme-input").val().trim();
@@ -42,6 +35,7 @@ $(document).ready(function () {
         addButtons();
     });
 
+    // adds new button with user input
     function addButtons() {
         $("#buttons").empty();
         for (var i = 0; i < memes.length; i++) {
@@ -53,9 +47,9 @@ $(document).ready(function () {
         }
     }
 
+    // calling the displayMemes function on meme button click
     $(document).on("click", ".meme-btn", displayMemes);
-
-
+    // adding buttons for all memes in the original memes array
     addButtons();
 
 });
