6b0441ac0de4b081a539fcd69a808159a74dbd37
lgfahys <lindsey@codeasite.com>
2019-03-14 22:46:41 -0400

    got 10 meme images and ratings to appear on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,19 +5,34 @@
 // on gif click make gif animate, on second click make static
 
 // add form for user to add a new meme category
-$( document ).ready(function() {
-var memes = ["kermit the frog", "the office", "grumpy cat", "pepe the frog", "mr. krabs", "doge"];
 
+$( document ).ready(function() {
+
+// creating a memes array to hold all meme categories
+var memes = ["kermit", "the office", "grumpy cat", "pepe the frog", "mr. krabs", "doge"];
+
+// creating a function
 function displayMemes() {
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5WvKlaf4SS3q9eGvInak6z3LoqlHOkgN&q=memes" + meme + "&limit=10";
 
 var meme = $(this).attr("data-name");
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5WvKlaf4SS3q9eGvInak6z3LoqlHOkgN&q=memes" + meme + "&limit=10";
 
 $.ajax({
     url: queryURL,
     method: "GET"
+
 }).then(function (response) {
 
+    // looping through 10 gifs and appending them to the memes div
+    for (var i = 0; i < 10; i++) {
+    console.log(response.data[i].rating);
+    var rating = response.data[i].rating;
+    var ratingDisplay = $("<p>").text("Rating: " + rating);
+    $("#memes").append(ratingDisplay);
+    var gifURL = response.data[i].images.original_still.url;
+    var gif = $("<img>").attr("src", gifURL);
+    $("#memes").append(gif);
+    }
 });
 }
 
@@ -42,6 +57,7 @@ function addButtons() {
 $(document).on("click", ".meme-btn", displayMemes);
 
 addButtons();
+
 });
 
 
